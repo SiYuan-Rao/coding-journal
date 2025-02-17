0353bc29229f7deff076c2de968fc5679112ead5
SiYuan-Rao <794280507@qq.com>
2025-02-17 15:43:11 +0800

    MOOC-浙江大学-翁恺-C语言程序设计-4-2-打印九九口诀表

---
+++ MOOC-浙江大学-翁恺-C语言程序设计/4-2-打印九九口诀表/answer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int i = 1;
+    while (i <= n) {
+        int j = 1;
+        while (j <= i) {
+            printf("%d*%d=%d", j, i, i * j);
+
+            if (i * j < 10){
+                printf("   ");
+            } else {
+                printf("  ");
+            }
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
